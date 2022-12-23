d098c7150804aecd0b372f96a38c6da909d2c5f6
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-12-23 06:07:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import './style.css'
+
+const openMenu = document.getElementById("openMenu")
+const closeMenu = document.getElementById("closeMenu")
+const menubar = document.querySelector("[role='menuBar']")
+
+openMenu.addEventListener('click',()=>{
+    const IsExpanded = JSON.parse(openMenu.getAttribute('aria-expanded'))
+    openMenu.setAttribute('aria-expanded', !IsExpanded)
+    navToggle()
+})
+
+closeMenu.addEventListener('click',()=>{
+    const IsExpanded = JSON.parse(closeMenu.getAttribute('aria-expanded'))
+    closeMenu.setAttribute('aria-expanded', !IsExpanded)
+    navToggle()
+})
+
+// function to toggle nav in mobile view
+function navToggle(){
+    openMenu.classList.toggle('hidden')
+    closeMenu.classList.toggle('hidden')
+    menubar.classList.toggle('hidden')
+    menubar.classList.toggle('flex')
+}
