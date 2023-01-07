5c7c1a4a7176b3e28707dde8ce15d1906d0f482c
Pulkit Saxena <pulkitsaxena795@gmail.com>
2023-01-07 11:04:55 +0530

    indentation

--- leaflet.js
+++ leaflet.js
@@ -18,74 +18,74 @@ const orderoid = [
     },
 ];
   
-  // checking if the trackId is in the order list 
-  let len = orderoid.length, i = 0;
-  while(len--) {
-    if(orderoid[i].oid == track){
-        document.getElementById("result").value = `${track}`
+// checking if the trackId is in the order list 
+let len = orderoid.length, i = 0;
+while(len--) {
+if(orderoid[i].oid == track){
+    document.getElementById("result").value = `${track}`
 
-        // Map initial focus point
-        var map = L.map('map').setView([30.6942, 76.8606], 6);
-    
-        // osm layer
-        var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        })
-    
-        osm.addTo(map)
-    
-        // * getting the geoLocation
-        // if(!navigator.geolocation){
-        //     alert("Your system doesnt support geolocation")
-        // }else{
-        //     setInterval(() => {
-        //         navigator.geolocation.getCurrentPosition(getPosition)
-        //     }, 5000);
-        // }
-    
-        var marker,circle;
-    
-        function getPosition(){
-    
-            // console.log(position)
-            // var lat = position.coords.latitude
-            // var long = position.coords.longitude
-            // var accuracy = position.coords.accuracy
-    
-            // console.log(lat,long,accuracy)
-    
-            if(marker){
-                map.removeLayer(marker)
-            }
-            if(circle){
-                map.removeLayer(circle)
-            }
-    
-            // console.log(orderoid[i].lat)
-    
-            marker = 
-            L.marker([orderoid[i].lat,orderoid[i].long]).addTo(map)
-                .bindPopup('Your courier is here.')
-                .openPopup();
-            
-            console.log(orderoid[i].lat)
-            console.log(orderoid[i].long)
-            marker.addTo(map)
-            circle = L.circle([orderoid[i].lat,orderoid[i].long],{radius: orderoid[i].accuracy})
-            circle.addTo(map)
-    
-            
-            var Group = L.featureGroup([marker,circle])
-            Group.addTo(map)
-    
-            map.fitBounds(Group.getBounds())
-    
+    // Map initial focus point
+    var map = L.map('map').setView([30.6942, 76.8606], 6);
+
+    // osm layer
+    var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+
+    osm.addTo(map)
+
+    // * getting the geoLocation
+    // if(!navigator.geolocation){
+    //     alert("Your system doesnt support geolocation")
+    // }else{
+    //     setInterval(() => {
+    //         navigator.geolocation.getCurrentPosition(getPosition)
+    //     }, 5000);
+    // }
+
+    var marker,circle;
+
+    function getPosition(){
+
+        // console.log(position)
+        // var lat = position.coords.latitude
+        // var long = position.coords.longitude
+        // var accuracy = position.coords.accuracy
+
+        // console.log(lat,long,accuracy)
+
+        if(marker){
+            map.removeLayer(marker)
         }
-        getPosition()
-        break;
+        if(circle){
+            map.removeLayer(circle)
+        }
+
+        // console.log(orderoid[i].lat)
+
+        marker = 
+        L.marker([orderoid[i].lat,orderoid[i].long]).addTo(map)
+            .bindPopup('Your courier is here.')
+            .openPopup();
+        
+        console.log(orderoid[i].lat)
+        console.log(orderoid[i].long)
+        marker.addTo(map)
+        circle = L.circle([orderoid[i].lat,orderoid[i].long],{radius: orderoid[i].accuracy})
+        circle.addTo(map)
+
+        
+        var Group = L.featureGroup([marker,circle])
+        Group.addTo(map)
+
+        map.fitBounds(Group.getBounds())
+
     }
-    i++;
-  }
+    getPosition()
+    break;
+}
+i++;
+}
 
 if(localStorage.getItem("trackingNum") == ''){
     alert("Please enter a tracking Number")
